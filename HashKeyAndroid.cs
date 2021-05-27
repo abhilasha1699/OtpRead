23bfba8cdffad5142364cbe776b16270b257eebf
abhilasha1699 <77963939+abhilasha1699@users.noreply.github.com>
2021-05-27 16:44:01 +0530

    Add files via upload

---
+++ HashKeyAndroid.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Auth.Api.Phone;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using OtpAutofill.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(HashKeyAndroid))]
+namespace OtpAutofill.Droid
+{
+    public class HashKeyAndroid : IHashKeyService
+    {
+        [Obsolete]
+        public string GenerateHashKey()
+        {
+            //throw new NotImplementedException();
+            return new AppHashKeyHelper().GetAppHashKey(global::Android.App.Application.Context);
+        }
+
+        public void StartSmsRetRec()
+        {
+            //throw new NotImplementedException();
+            //SmsRetrieverClient smsRetrieverClient = SmsRetrieverClient.getClient(this);
+            SmsRetrieverClient _client = SmsRetriever.GetClient(Android.App.Application.Context);
+            var task = _client.StartSmsRetriever();
+        }
+    }
+}
---
+++ SmsBroadcastReceiver.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Auth.Api.Phone;
+using Android.Gms.Common.Apis;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace OtpAutofill.Droid
+{
+    [BroadcastReceiver]
+    [IntentFilter(new[] { SmsRetriever.SmsRetrievedAction})]
+    public class SmsBroadcastReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            //Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+            if (intent.Action != SmsRetriever.SmsRetrievedAction)
+            {
+                return;
+            }
+            var extrasBundle = intent.Extras;
+            if (extrasBundle == null)
+            {
+                return;
+            }
+            var status = (Statuses)extrasBundle.Get(SmsRetriever.ExtraStatus);
+            switch (status.StatusCode)
+            {
+                case CommonStatusCodes.Success:
+                    var messageContent = (string)extrasBundle.Get(SmsRetriever.ExtraSmsMessage);
+                    MessagingCenter.Send<string>(messageContent, "ReceivedOTP");
+                    break;
+
+                case CommonStatusCodes.Timeout:
+                    break;
+            }
+        }
+    }
+}
